b169d9008ac014df29678d7a0b1ffc548b1b26e5
MegaBossMan <megabossman50@gmail.com>
2021-10-21 21:36:28 -0400

    Updated Charge Kick's data, functions, and sprites

--- charge-kick/functions.php
+++ charge-kick/functions.php
@@ -7,31 +7,52 @@ $functions = array(
 
         // Target the opposing robot
         $this_ability->target_options_update(array(
-            'frame' => 'shoot',
-            'success' => array(0, 75, 0, 10, $this_robot->print_name().' uses the '.$this_ability->print_name().'!')
+            'frame' => 'slide',
+            'kickback' => array(160, 0, 0),
+            'success' => array(0, 12, 0, 10, $this_robot->print_name().' uses '.$this_ability->print_name().'!')
             ));
         $this_robot->trigger_target($target_robot, $this_ability);
 
         // Inflict damage on the opposing robot
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
-            'kickback' => array(10, 0, 0),
-            'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_name().' hit the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'kickback' => array(60, 0, 0),
+            'rates' => array('auto', 'auto', $this_ability->ability_recovery2),
+            'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_name().' crashes into the target!'),
+            'failure' => array(1, -65, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
             'frame' => 'taunt',
-            'kickback' => array(10, 0, 0),
+            'kickback' => array(20, 0, 0),
+            'rates' => array('auto', 'auto', $this_ability->ability_recovery2),
             'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_name().' was absorbed by the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'failure' => array(1, -65, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
             ));
         $energy_damage_amount = $this_ability->ability_damage;
-        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+        $trigger_options = array('target_stat_substitution' => array('robot_attack' => 'robot_speed'));
+        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, true, $trigger_options);
+        
+                // Call the global stat break function with customized options
+        rpg_ability::ability_function_stat_break($this_robot, 'speed', 1, $this_ability);
+
+        // Return true on success
+        return true;
+       
+
+        },
+    'ability_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // If the user has Extended Range, allow bench targeting
+        if ($this_robot->has_attribute('extended-range')){ $this_ability->set_target('select_target'); }
+        else { $this_ability->reset_target(); }
 
         // Return true on success
         return true;
 
-    }
+        }
 );
 ?>
