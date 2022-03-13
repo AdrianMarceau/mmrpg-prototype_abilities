a8022b2e70324abdea30fcdd07c75a57e02ffa6a
MegaBossMan <megabossman50@gmail.com>
2022-03-13 19:08:53 -0400

    Updated Shadow Buster's functions

--- shadow-buster/functions.php
+++ shadow-buster/functions.php
@@ -1,7 +1,7 @@
 <?
 $functions = array(
 	'ability_function' => function($objects){
-		return rpg_ability::ability_function_elemental_buster($objects, 'fire', 'incinerated', 'superheated');
+		return rpg_ability::ability_function_elemental_buster($objects, 'shadow', 'distressed', 'energized');
 	},
 	'ability_function_onload' => function($objects){
 		return rpg_ability::ability_function_elemental_buster_onload($objects);
