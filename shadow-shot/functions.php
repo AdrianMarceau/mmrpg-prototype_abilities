7f7c08b1e6c2144e0682ad778c9d0a10b9d2bdea
MegaBossMan <megabossman50@gmail.com>
2022-03-13 19:08:53 -0400

    Updated Shadow Shot's functions

--- shadow-shot/functions.php
+++ shadow-shot/functions.php
@@ -1,7 +1,7 @@
 <?
 $functions = array(
     'ability_function' => function($objects){
-        return rpg_ability::ability_function_elemental_shot($objects, 'fire', 'burned', 'ignited');
+        return rpg_ability::ability_function_elemental_shot($objects, 'shadow', 'agonized', 'energized');
     },
     'ability_function_onload' => function($objects){
         return rpg_ability::ability_function_elemental_shot_onload($objects);
