3bd1a58d9d9d75d67210499a660a9dc7f0660a6c
MegaBossMan <megabossman50@gmail.com>
2022-03-13 19:08:53 -0400

    Updated Shadow Overdrive's functions

--- shadow-overdrive/functions.php
+++ shadow-overdrive/functions.php
@@ -1,7 +1,7 @@
 <?
 $functions = array(
     'ability_function' => function($objects){
-        return rpg_ability::ability_function_elemental_overdrive($objects, 'fire', 'incinerated', 'superheated');
+        return rpg_ability::ability_function_elemental_overdrive($objects, 'shadow', 'distressed', 'energized');
     },
     'ability_function_onload' => function($objects){
         return rpg_ability::ability_function_elemental_overdrive_onload($objects);
