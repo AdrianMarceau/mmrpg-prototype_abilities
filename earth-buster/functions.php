629badfe900592bb92c72d5b0037608b687eeee7
MegaBossMan <megabossman50@gmail.com>
2022-03-13 19:08:53 -0400

    Updated Earth Buster's data, functions, and sprites

---
+++ earth-buster/functions.php
@@ -0,0 +1,10 @@
+<?
+$functions = array(
+	'ability_function' => function($objects){
+		return rpg_ability::ability_function_elemental_buster($objects, 'earth', 'rocked', 'unearthed');
+	},
+	'ability_function_onload' => function($objects){
+		return rpg_ability::ability_function_elemental_buster_onload($objects);
+	}
+);
+?>
