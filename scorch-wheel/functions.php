2c8293935f3d6c86584c9a46d6db52898d5c03fe
MegaBossMan <megabossman50@gmail.com>
2022-03-16 22:52:48 -0400

    Updated Scorch Wheel's data and functions

--- scorch-wheel/functions.php
+++ scorch-wheel/functions.php
@@ -4,6 +4,17 @@ $functions = array(
 
         // Extract all objects into the current scope
         extract($objects);
+        
+                // Update this ability's damage based on the user and target's speed
+        if (!empty($target_robot->counters['speed_mods'])
+            && $target_robot->counters['speed_mods'] < 0){
+            $new_damage_amount = $this_ability->ability_base_damage + ($target_robot->counters['speed_mods'] * 5);
+            $this_ability->set_name($this_ability->ability_base_name.' Δ');
+            $this_ability->set_damage($new_damage_amount);
+        } else {
+            $this_ability->reset_name();
+            $this_ability->reset_damage();
+        }
 
         // Target the opposing robot
         $this_ability->target_options_update(array(
@@ -27,7 +38,21 @@ $functions = array(
             'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
             ));
         $energy_damage_amount = $this_ability->ability_damage;
-        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+        $trigger_options = array('target_stat_substitution' => array('robot_attack' => 'robot_speed'));
+        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, true, $trigger_options);
+        
+        // Update this ability's damage based on the user and target's speed
+        if (!empty($target_robot)){
+            if (!empty($target_robot->counters['speed_mods'])
+                && $target_robot->counters['speed_mods'] > 0){
+                $new_damage_amount = $this_ability->ability_base_damage + ($target_robot->counters['speed_mods'] * 5);
+                $this_ability->set_name($this_ability->ability_base_name.' Δ');
+                $this_ability->set_damage($new_damage_amount);
+            } else {
+                $this_ability->reset_name();
+                $this_ability->reset_damage();
+            }
+        }
 
         // Return true on success
         return true;
