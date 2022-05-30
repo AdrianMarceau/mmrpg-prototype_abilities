1a137053df6bfa47b4948c35cbd43af165d894a3
MegaBossMan <megabossman50@gmail.com>
2022-05-29 21:15:44 -0400

    Updated Grab Buster's data, functions, and sprites

--- grab-buster/functions.php
+++ grab-buster/functions.php
@@ -8,7 +8,8 @@ $functions = array(
         // Target the opposing robot
         $this_ability->target_options_update(array(
             'frame' => 'shoot',
-            'success' => array(0, 75, 0, 10, $this_robot->print_name().' uses the '.$this_ability->print_name().'!')
+            'kickback' => array(-10, 0, 0),
+            'success' => array(0, 150, 0, 10, $this_robot->print_name().' fires a '.$this_ability->print_name().'!')
             ));
         $this_robot->trigger_target($target_robot, $this_ability);
 
@@ -16,22 +17,64 @@ $functions = array(
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
             'kickback' => array(10, 0, 0),
-            'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_name().' hit the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'success' => array(1, -100, 0, 10, 'The '.$this_ability->print_name(true).' drained the target\'s energy!'),
+            'failure' => array(1, -125, 0, -10, 'The '.$this_ability->print_name(true).' missed the target&hellip;')
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
             'frame' => 'taunt',
             'kickback' => array(10, 0, 0),
-            'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_name().' was absorbed by the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'success' => array(1, -75, 0, 10, 'The '.$this_ability->print_name(true).' emboldened the target!'),
+            'failure' => array(1, -100, 0, -10, 'The '.$this_ability->print_name(true).' missed the target&hellip;')
             ));
         $energy_damage_amount = $this_ability->ability_damage;
         $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
 
+        // Restore life energy if the ability was successful
+        if ($this_robot->robot_energy < $this_robot->robot_base_energy
+            && $this_ability->ability_results['this_result'] != 'failure'
+            && $this_ability->ability_results['this_amount'] > 0){
+
+            // Increase the target robot's energy stat
+            $this_ability->recovery_options_update(array(
+                'kind' => 'energy',
+                'frame' => 'taunt',
+                'percent' => true,
+                'kickback' => array(0, 0, 0),
+                'success' => array(2, -5, -5, 10, $this_robot->print_name().'\'s life energy was restored!'),
+                'failure' => array(2, 0, 0, -9999, '')
+                ));
+            $this_ability->damage_options_update(array(
+                'kind' => 'energy',
+                'frame' => 'defend',
+                'percent' => true,
+                'kickback' => array(0, 0, 0),
+                'success' => array(2, -5, -5, -10, $this_robot->print_name().'\'s life energy was lowered!'),
+                'failure' => array(2, 0, 0, -9999, '')
+                ));
+            $energy_recovery_amount = ceil($this_ability->ability_results['this_amount'] * ($this_ability->ability_recovery2 / 100));
+            if ($this_robot->robot_energy + $energy_recovery_amount > $this_robot->robot_base_energy){ $energy_recovery_amount = $this_robot->robot_base_energy - $this_robot->robot_energy; }
+            $trigger_options = array('apply_modifiers' => false);
+            $this_robot->trigger_recovery($this_robot, $this_ability, $energy_recovery_amount, true, $trigger_options);
+
+        }
+
         // Return true on success
         return true;
 
-    }
+        },
+    'ability_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // If the user has Extended Range, allow bench targeting
+        if ($this_robot->has_attribute('extended-range')){ $this_ability->set_target('select_target'); }
+        else { $this_ability->reset_target(); }
+
+        // Return true on success
+        return true;
+
+        }
 );
 ?>
