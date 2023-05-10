dd3d473f62a7b18cee6bd65c3217afd420d8a697
Ageman20XX <adrian.marceau@gmail.com>
2023-05-10 15:14:16 -0400

    Updated Galaxy Bomb's functions

--- galaxy-bomb/functions.php
+++ galaxy-bomb/functions.php
@@ -16,7 +16,8 @@ $functions = array(
         // Define this ability's attachment token
         $static_attachment_key = $target_robot->get_static_attachment_key();
         $static_attachment_duration = 5;
-        $this_attachment_info = rpg_ability::get_static_black_hole($static_attachment_key, $static_attachment_duration);
+        //$this_attachment_info = rpg_ability::get_static_black_hole($static_attachment_key, $static_attachment_duration);
+        $this_attachment_info = rpg_ability::get_static_attachment($this_ability->ability_token, 'black-hole', $static_attachment_key, $static_attachment_duration);
         $this_attachment_token = $this_attachment_info['attachment_token'];
 
         // Target the opposing robot
@@ -86,6 +87,76 @@ $functions = array(
         // Return true on success
         return true;
 
-        }
+        },
+    'static_attachment_function_black-hole' => function($objects, $static_attachment_key, $this_attachment_duration = 99){
+
+        // Extract all objects and config into the current scope
+        extract($objects);
+        
+        // Generate the static attachment info using provided config
+        $existing_attachments = isset($this_battle->battle_attachments[$static_attachment_key]) ? count($this_battle->battle_attachments[$static_attachment_key]) : 0;
+        $this_ability_token = $this_ability->ability_token;
+        $this_attachment_token = 'ability_'.$this_ability_token.'_'.$this_attachment->attachment_token.'_'.$static_attachment_key;
+        $this_attachment_image = $this_ability_token;
+        $this_attachment_destroy_text = 'The crushing <span class="ability_name ability_type ability_type_space_explode">Black Hole</span> behind {this_robot} faded away... ';
+        $this_attachment_repeat_text = 'The <span class="ability_name ability_type ability_type_space_explode">Black Hole</span> behind {this_robot} exherted its power! ';
+        $this_attachment_info = array(
+            'class' => 'ability',
+            'sticky' => true,
+            'ability_token' => $this_ability_token,
+            'ability_image' => $this_attachment_image,
+            'attachment_token' => $this_attachment_token,
+            'attachment_duration' => $this_attachment_duration,
+            'attachment_energy' => 0,
+            'attachment_energy_base_percent' => 10,
+            'attachment_sticky' => true,
+            'attachment_destroy' => array(
+                'trigger' => 'special',
+                'kind' => '',
+                'type' => '',
+                'percent' => true,
+                'modifiers' => false,
+                'frame' => 'defend',
+                'rates' => array(100, 0, 0),
+                'success' => array(9, -9999, -9999, 10, $this_attachment_destroy_text),
+                'failure' => array(9, -9999, -9999, 10, $this_attachment_destroy_text)
+                ),
+            'attachment_repeat' => array(
+                'kind' => 'energy',
+                'trigger' => 'damage',
+                'type' => 'space',
+                'type2' => 'explode',
+                'energy' => 10,
+                'percent' => true,
+                'modifiers' => true,
+                'frame' => 'damage',
+                'rates' => array(100, 0, 0),
+                'success' => array(1, -5, 5, -10, $this_attachment_repeat_text),
+                'failure' => array(1, -5, 5, -99, $this_attachment_repeat_text),
+                'options' => array(
+                    'apply_modifiers' => true,
+                    'apply_type_modifiers' => true,
+                    'apply_core_modifiers' => true,
+                    'apply_field_modifiers' => true,
+                    'apply_stat_modifiers' => false,
+                    'apply_position_modifiers' => false,
+                    'referred_damage' => true,
+                    'referred_damage_id' => 0,
+                    'referred_damage_stats' => array()
+                    )
+                ),
+            'ability_frame' => 1,
+            'ability_frame_animate' => array(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            'ability_frame_offset' => array(
+                'x' => (-5 + ($existing_attachments * 6)),
+                'y' => (5 + $existing_attachments),
+                'z' => (-10 - $existing_attachments)
+                )
+            );
+
+        // Return true on success
+        return $this_attachment_info;
+
+    }
 );
 ?>
