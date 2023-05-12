0f2727d1f42d62d3b060327ce6ac3800e0a5f604
Ageman20XX <adrian.marceau@gmail.com>
2023-05-12 14:09:10 -0400

    Updated Thunder Wool's functions

--- thunder-wool/functions.php
+++ thunder-wool/functions.php
@@ -21,75 +21,11 @@ $functions = array(
         // Define this ability's attachment token
         $static_attachment_key = $target_robot->get_static_attachment_key();
         $static_attachment_duration = 3;
-        $this_attachment_info = rpg_ability::get_static_woolly_cloud($static_attachment_key, $static_attachment_duration);
+        $this_attachment_info = rpg_ability::get_static_attachment($this_ability, 'woolly-cloud', $static_attachment_key, $static_attachment_duration);
         $this_attachment_token = $this_attachment_info['attachment_token'];
 
         // Update the attachment image if a special robot is using it
         $this_attachment_info['ability_image'] = $this_ability->ability_image;
-
-        /*
-        $this_attachment_token = 'ability_'.$this_ability->ability_token.'_'.$static_attachment_key;
-        $this_attachment_info = array(
-            'class' => 'ability',
-            'sticky' => true,
-            'ability_id' => $this_ability->ability_id.'_'.$static_attachment_key,
-            'ability_token' => $this_ability->ability_token,
-            'ability_image' => $this_ability->ability_image,
-            'attachment_duration' => $static_attachment_duration,
-            'attachment_energy' => 0,
-            'attachment_energy_base_percent' => $this_ability->ability_damage,
-            'attachment_token' => $this_attachment_token,
-            'attachment_sticky' => true,
-            'attachment_create' => array(
-                'trigger' => 'special',
-                'kind' => '',
-                'percent' => true,
-                'frame' => 'defend',
-                'rates' => array(100, 0, 0),
-                'success' => array(9, -10, -5, -10, $this_create_text),
-                'failure' => array(9, -10, -5, -10, $this_create_text)
-                ),
-            'attachment_destroy' => array(
-                'trigger' => 'special',
-                'kind' => '',
-                'type' => '',
-                'type2' => '',
-                'percent' => true,
-                'modifiers' => false,
-                'frame' => 'taunt',
-                'rates' => array(100, 0, 0),
-                'success' => array(9, 0, -9999, 0,  $this_destroy_text),
-                'failure' => array(9, 0, -9999, 0, $this_destroy_text)
-                ),
-            'attachment_repeat' => array(
-                'kind' => 'energy',
-                'trigger' => 'damage',
-                'type' => $this_ability->ability_type,
-                'type2' => $this_ability->ability_type2,
-                'energy' => $this_ability->ability_damage,
-                'percent' => true,
-                'modifiers' => true,
-                'frame' => 'damage',
-                'rates' => array(100, 0, 0),
-                'success' => array(9, -5, 30, 10, $this_repeat_text),
-                'failure' => array(9, -5, 30, 10, $this_repeat_text),
-                'options' => array(
-                    'apply_modifiers' => true,
-                    'apply_type_modifiers' => true,
-                    'apply_core_modifiers' => true,
-                    'apply_field_modifiers' => true,
-                    'apply_stat_modifiers' => false,
-                    'apply_position_modifiers' => false,
-                    'referred_damage' => true,
-                    'referred_damage_id' => $this_robot->robot_id,
-                    'referred_damage_stats' => $this_robot->get_stats()
-                    )
-                ),
-            'ability_frame' => 0,
-            'ability_frame_animate' => array(0, 2, 4, 1, 3, 5),
-            'ability_frame_offset' => array('x' => -5, 'y' => 40, 'z' => -8)
-            );
-        */
 
         // Target the opposing robot
         if ($this_robot->robot_token == 'sheep-man'){
@@ -160,6 +96,76 @@ $functions = array(
         // Return true on success
         return true;
 
-        }
+        },
+    'static_attachment_function_woolly-cloud' => function($objects, $static_attachment_key, $this_attachment_duration = 99){
+
+        // Extract all objects and config into the current scope
+        extract($objects);
+        
+        // Generate the static attachment info using provided config
+        $existing_attachments = isset($this_battle->battle_attachments[$static_attachment_key]) ? count($this_battle->battle_attachments[$static_attachment_key]) : 0;
+        $this_ability_token = $this_ability->ability_token;
+        $this_attachment_token = 'ability_'.$this_ability_token.'_'.$this_attachment->attachment_token.'_'.$static_attachment_key;
+        $this_attachment_image = $this_ability_token;
+        $this_attachment_destroy_text = 'The thunderous '.rpg_type::print_span('electric', 'Woolly Cloud').' above {this_robot} faded away... ';
+        $this_attachment_repeat_text = 'The '.rpg_type::print_span('electric', 'Woolly Cloud').' above {this_robot} released a lightning bolt! ';
+        $this_attachment_info = array(
+            'class' => 'ability',
+            'sticky' => true,
+            'ability_token' => $this_ability_token,
+            'ability_image' => $this_attachment_image,
+            'attachment_token' => $this_attachment_token,
+            'attachment_duration' => $this_attachment_duration,
+            'attachment_energy' => 0,
+            'attachment_energy_base_percent' => 15,
+            'attachment_sticky' => true,
+            'attachment_destroy' => array(
+                'trigger' => 'special',
+                'kind' => '',
+                'type' => '',
+                'percent' => true,
+                'modifiers' => false,
+                'frame' => 'defend',
+                'rates' => array(100, 0, 0),
+                'success' => array(9, -9999, -9999, 10, $this_attachment_destroy_text),
+                'failure' => array(9, -9999, -9999, 10, $this_attachment_destroy_text)
+                ),
+            'attachment_repeat' => array(
+                'kind' => 'energy',
+                'trigger' => 'damage',
+                'type' => 'electric',
+                'type2' => '',
+                'energy' => 15,
+                'percent' => true,
+                'modifiers' => true,
+                'frame' => 'damage',
+                'rates' => array(100, 0, 0),
+                'success' => array(9, -5, 30, 10, $this_attachment_repeat_text),
+                'failure' => array(9, -5, 30, 10, $this_attachment_repeat_text),
+                'options' => array(
+                    'apply_modifiers' => true,
+                    'apply_type_modifiers' => true,
+                    'apply_core_modifiers' => true,
+                    'apply_field_modifiers' => true,
+                    'apply_stat_modifiers' => false,
+                    'apply_position_modifiers' => false,
+                    'referred_damage' => true,
+                    'referred_damage_id' => 0,
+                    'referred_damage_stats' => array()
+                    )
+                ),
+            'ability_frame' => 0,
+            'ability_frame_animate' => array(0, 2, 4, 1, 3, 5),
+            'ability_frame_offset' => array(
+                'x' => (-5 + ($existing_attachments * 2)),
+                'y' => (40 + ($existing_attachments * 1)),
+                'z' => (-8 - $existing_attachments)
+                )
+            );    
+
+        // Return true on success
+        return $this_attachment_info;
+
+    }
 );
 ?>
