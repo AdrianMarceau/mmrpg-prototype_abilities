4b41af3634f9634d8ebcc2b16a706f90c569c135
Ageman20XX <adrian.marceau@gmail.com>
2023-06-19 03:37:20 -0400

    Updated Attack Support's data and functions

--- attack-support/functions.php
+++ attack-support/functions.php
@@ -10,21 +10,27 @@ $functions = array(
         $this_robot->trigger_target($this_robot, $this_ability);
 
         // Call the global stat boost function with customized options
-        rpg_ability::ability_function_stat_boost($this_robot, 'attack', 1, $this_ability);
+        rpg_ability::ability_function_stat_boost($this_robot, 'attack', $this_ability->ability_recovery2, $this_ability);
 
         // Loop through this player's active bots and raise their stats
         $backup_robots_active = $this_player->values['robots_active'];
         $backup_robots_active_count = !empty($backup_robots_active) ? count($backup_robots_active) : 0;
         if ($backup_robots_active_count > 0){
+            $this_robot->set_frame_styles('transform: scaleX(-1);');
             $this_key = 0;
+            $this_frame_key = 0;
             foreach ($backup_robots_active AS $key => $info){
                 if ($info['robot_id'] == $this_robot->robot_id){ continue; }
+            		$this_robot->set_frame($this_frame_key % 2 === 0 ? 'summon' : 'taunt');
                 $temp_this_robot = rpg_game::get_robot($this_battle, $this_player, $info);
-                rpg_ability::ability_function_stat_boost($temp_this_robot, 'attack', 1, $this_ability, array(
+                rpg_ability::ability_function_stat_boost($temp_this_robot, 'attack', $this_ability->ability_recovery2, $this_ability, array(
                     'initiator_robot' => $this_robot
                     ));
                 $this_key++;
+                $this_frame_key++;
             }
+            $this_robot->reset_frame_styles();
+            $this_robot->reset_frame();
         }
 
         // Return true on success
