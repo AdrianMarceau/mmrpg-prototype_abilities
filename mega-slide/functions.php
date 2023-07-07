1c33988eb69b5b57f667f205fb0437f86fe6647e
Ageman20XX <adrian.marceau@gmail.com>
2023-07-07 17:18:31 -0400

    Updated Mega Slide's functions

--- mega-slide/functions.php
+++ mega-slide/functions.php
@@ -6,12 +6,16 @@ $functions = array(
         extract($objects);
 
         // Target the opposing robot
+        $trigger_options = array();
+        $trigger_options['event_flag_sound_effects'] = array(
+            array('name' => 'hyper-slide-sound', 'volume' => 1.5),
+            );
         $this_ability->target_options_update(array(
             'frame' => 'slide',
             'kickback' => array(150, 0, 0),
             'success' => array(0, 25, 0, -10, $this_robot->print_name().' uses '.$this_ability->print_name().'!')
             ));
-        $this_robot->trigger_target($target_robot, $this_ability);
+        $this_robot->trigger_target($target_robot, $this_ability, $trigger_options);
 
         // Inflict damage on the opposing robot
         $this_ability->damage_options_update(array(
