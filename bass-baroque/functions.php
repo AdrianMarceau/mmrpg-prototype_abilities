9bc0505fc7c79d4bc4d1880b41c0cf6f42428541
Ageman20XX <adrian.marceau@gmail.com>
2023-07-07 17:18:30 -0400

    Updated Bass Baroque's functions

--- bass-baroque/functions.php
+++ bass-baroque/functions.php
@@ -6,12 +6,16 @@ $functions = array(
         extract($objects);
 
         // Target the opposing robot
+        $trigger_options = array();
+        $trigger_options['event_flag_sound_effects'] = array(
+            array('name' => 'hyper-summon-sound', 'volume' => 1.5),
+            );
         $this_ability->target_options_update(array(
             'frame' => 'summon',
             'kickback' => array(0, 0, 0),
             'success' => array(1, -10, 0, -10, $this_robot->print_name().' summons the '.$this_ability->print_name().'!')
             ));
-        $this_robot->trigger_target($target_robot, $this_ability);
+        $this_robot->trigger_target($target_robot, $this_ability, $trigger_options);
 
         // Inflict damage on the opposing robot
         $this_ability->damage_options_update(array(
