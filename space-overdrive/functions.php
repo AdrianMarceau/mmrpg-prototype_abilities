53629ffc369feeb298063048e0d58d87dd7a7263
Ageman20XX <adrian.marceau@gmail.com>
2023-07-14 15:44:29 -0400

    Updated Space Overdrive's functions

--- space-overdrive/functions.php
+++ space-overdrive/functions.php
@@ -1,6 +1,10 @@
 <?
 $functions = array(
     'ability_function' => function($objects){
+        extract($objects);
+        $this_battle->queue_sound_effect('cosmic-sound');
+        $this_battle->queue_sound_effect(array('name' => 'cosmic-sound', 'delay' => 100));
+        $this_battle->queue_sound_effect(array('name' => 'cosmic-sound', 'delay' => 200));
         return rpg_ability::ability_function_elemental_overdrive($objects, 'cosmic', 'discorded', 'harmonized');
     },
     'ability_function_onload' => function($objects){
