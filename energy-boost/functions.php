bb394f5725a5ea1c029444b65bc89e6c53cd9cfe
Ageman20XX <adrian.marceau@gmail.com>
2023-07-14 15:44:28 -0400

    Updated Energy Boost's functions

--- energy-boost/functions.php
+++ energy-boost/functions.php
@@ -11,6 +11,7 @@ $functions = array(
 
         // If the target has already been disabled, we cannot continue
         if ($temp_target_robot->robot_status == 'disabled'){
+            $this_battle->queue_sound_effect('no-effect');
             $this_ability->target_options_update(array(
                 'frame' => 'defend',
                 'success' => array(0, -10, 40, -15,
@@ -23,14 +24,12 @@ $functions = array(
         }
 
         // Target this robot's self
-        $target_options = array();
-        $target_options['event_flag_sound_effects'] = array(
-            array('name' => 'summon-positive', 'volume' => 1.25)
-            );
+        $this_battle->queue_sound_effect('summon-positive');
         $this_ability->target_options_update(array(
             'frame' => 'summon',
             'success' => array(0, 0, 10, -10, $this_robot->print_name().' uses the '.$this_ability->print_name().' technique!')
             ));
+        $target_options = array();
         $this_robot->trigger_target($this_robot, $this_ability, $target_options);
 
         // If the target of this ability is not the user
