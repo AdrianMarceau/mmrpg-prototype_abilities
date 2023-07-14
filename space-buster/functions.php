035e46d6cabf36e3741ba7e5fe7454c04b8db06c
Ageman20XX <adrian.marceau@gmail.com>
2023-07-14 15:44:29 -0400

    Updated Space Buster's functions

--- space-buster/functions.php
+++ space-buster/functions.php
@@ -1,6 +1,8 @@
 <?
 $functions = array(
     'ability_function' => function($objects){
+        extract($objects);
+        $this_battle->queue_sound_effect('cosmic-sound');
         return rpg_ability::ability_function_elemental_buster($objects, 'cosmic', 'discorded', 'harmonized');
     },
     'ability_function_onload' => function($objects){
