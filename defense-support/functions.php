da466315f1a3ba68e09d78c9240c034bf04767d7
Ageman20XX <adrian.marceau@gmail.com>
2023-07-14 15:44:28 -0400

    Updated Defense Support's functions

--- defense-support/functions.php
+++ defense-support/functions.php
@@ -6,6 +6,7 @@ $functions = array(
         extract($objects);
 
         // Target this robot's self
+        $this_battle->queue_sound_effect('summon-positive');
         $this_ability->target_options_update(array('frame' => 'summon', 'success' => array(0, 0, 0, -10, $this_robot->print_name().' uses '.$this_ability->print_name().'!')));
         $this_robot->trigger_target($this_robot, $this_ability);
 
@@ -21,7 +22,8 @@ $functions = array(
             $this_frame_key = 0;
             foreach ($backup_robots_active AS $key => $info){
                 if ($info['robot_id'] == $this_robot->robot_id){ continue; }
-            		$this_robot->set_frame($this_frame_key % 2 === 0 ? 'summon' : 'taunt');
+                $this_battle->queue_sound_effect(array('name' => 'summon-positive', 'volume' => 0.3));
+            	$this_robot->set_frame($this_frame_key % 2 === 0 ? 'summon' : 'taunt');
                 $temp_this_robot = rpg_game::get_robot($this_battle, $this_player, $info);
                 rpg_ability::ability_function_stat_boost($temp_this_robot, 'defense', $this_ability->ability_recovery2, $this_ability, array(
                     'initiator_robot' => $this_robot
