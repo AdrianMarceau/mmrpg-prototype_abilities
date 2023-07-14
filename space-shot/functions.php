293482c6a7601b45c29f0ea7afe1c1ca3c35aa7f
Ageman20XX <adrian.marceau@gmail.com>
2023-07-14 15:44:29 -0400

    Updated Space Shot's functions

--- space-shot/functions.php
+++ space-shot/functions.php
@@ -1,6 +1,8 @@
 <?
 $functions = array(
     'ability_function' => function($objects){
+        extract($objects);
+        $this_battle->queue_sound_effect('cosmic-sound');
         return rpg_ability::ability_function_elemental_shot($objects, 'cosmic', 'doomed', 'blessed');
     },
     'ability_function_onload' => function($objects){
