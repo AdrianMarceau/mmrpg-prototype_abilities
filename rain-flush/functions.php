d34e756a6a041fc876e86208ae212362a14205a1
Ageman20XX <adrian.marceau@gmail.com>
2023-07-21 16:10:34 -0400

    Updated Rain Flush's functions

--- rain-flush/functions.php
+++ rain-flush/functions.php
@@ -26,13 +26,11 @@ $functions = array(
         $this_ability->update_session();
 
         // Target the opposing robot
+        $this_battle->queue_sound_effect(array('name' => 'rain-sound'));
+        $this_battle->queue_sound_effect(array('name' => 'rain-sound', 'delay' => 200));
+        $this_battle->queue_sound_effect(array('name' => 'rain-sound', 'delay' => 400));
         $target_options = array();
         $target_options['prevent_stats_text'] = true;
-        $target_options['event_flag_sound_effects'] = array(
-            array('name' => 'rain-sound', 'volume' => 1.0),
-            array('name' => 'rain-sound', 'volume' => 1.0, 'delay' => 200),
-            array('name' => 'rain-sound', 'volume' => 1.0, 'delay' => 400)
-            );
         $this_ability->target_options_update(array(
             'frame' => 'summon',
             'success' => array(1, 10, 100, 10, $this_robot->print_name().' releases the '.$this_ability->print_name().'!')
