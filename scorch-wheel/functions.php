0d65d20f191a0ec958b9466bc511c0c9930ed16f
Ageman20XX <adrian.marceau@gmail.com>
2023-08-29 01:44:09 -0400

    Updated Scorch Wheel's data, functions, and sprites

--- scorch-wheel/functions.php
+++ scorch-wheel/functions.php
@@ -4,22 +4,18 @@ $functions = array(
 
         // Extract all objects into the current scope
         extract($objects);
-        
-                // Update this ability's damage based on the user and target's speed
-        if (!empty($target_robot->counters['speed_mods'])
-            && $target_robot->counters['speed_mods'] < 0){
-            $new_damage_amount = $this_ability->ability_base_damage + ($target_robot->counters['speed_mods'] * 5);
-            $this_ability->set_name($this_ability->ability_base_name.' Δ');
-            $this_ability->set_damage($new_damage_amount);
-        } else {
-            $this_ability->reset_name();
-            $this_ability->reset_damage();
-        }
+
+        // Target this robot's self
+        $this_ability->target_options_update(array(
+            'frame' => 'summon',
+            'success' => array(0, -10, 0, 10, $this_robot->print_name().' raises a shield of stars!')
+            ));
+        $this_robot->trigger_target($this_robot, $this_ability);
 
         // Target the opposing robot
         $this_ability->target_options_update(array(
-            'frame' => 'shoot',
-            'success' => array(0, 75, 0, 10, $this_robot->print_name().' uses the '.$this_ability->print_name().'!')
+            'frame' => 'throw',
+            'success' => array(1, 100, 0, 10, $this_robot->print_name().' throws the '.$this_ability->print_name().'!')
             ));
         $this_robot->trigger_target($target_robot, $this_ability);
 
@@ -27,31 +23,64 @@ $functions = array(
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
             'kickback' => array(10, 0, 0),
-            'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_name().' hit the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'success' => array(2, 0, 5, 10, 'The '.$this_ability->print_name().' collided with the target!'),
+            'failure' => array(0, -50, 5, -10, 'The '.$this_ability->print_name().' missed&hellip;')
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
             'frame' => 'taunt',
-            'kickback' => array(10, 0, 0),
-            'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_name().' was absorbed by the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'kickback' => array(0, 0, 0),
+            'success' => array(0, 0, 5, 10, 'The '.$this_ability->print_name().' invigorated the target!'),
+            'failure' => array(0, -50, 5, -10, 'The '.$this_ability->print_name().' missed&hellip;')
             ));
         $energy_damage_amount = $this_ability->ability_damage;
-        $trigger_options = array('target_stat_substitution' => array('robot_attack' => 'robot_speed'));
-        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, true, $trigger_options);
-        
-        // Update this ability's damage based on the user and target's speed
-        if (!empty($target_robot)){
-            if (!empty($target_robot->counters['speed_mods'])
-                && $target_robot->counters['speed_mods'] > 0){
-                $new_damage_amount = $this_ability->ability_base_damage + ($target_robot->counters['speed_mods'] * 5);
-                $this_ability->set_name($this_ability->ability_base_name.' Δ');
-                $this_ability->set_damage($new_damage_amount);
-            } else {
-                $this_ability->reset_name();
-                $this_ability->reset_damage();
+        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+
+        // If this attack returns and strikes a second time (random chance)
+        if ($this_ability->ability_results['this_result'] != 'failure'
+            && $target_robot->robot_status != 'disabled'){
+
+            // Inflict damage on the opposing robot
+            $this_ability->damage_options_update(array(
+                'kind' => 'energy',
+                'kickback' => array(20, 0, 0),
+                'success' => array(0, -40, 10, 10, 'Oh! The '.$this_ability->print_name().' hit again!'),
+                'failure' => array(0, -90, 10, -10, '')
+                ));
+            $this_ability->recovery_options_update(array(
+                'kind' => 'energy',
+                'kickback' => array(0, 0, 0),
+                'frame' => 'taunt',
+                'success' => array(1, -40, 10, 10, 'Oh no! Not again!'),
+                'failure' => array(1, -90, 10, -10, '')
+                ));
+            $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+
+            // If this attack returns and strikes a third time (random chance)
+            if ($this_ability->ability_results['this_result'] != 'failure'
+                && $target_robot->robot_energy != 'disabled'){
+
+                // Inflict damage on the opposing robot
+                $this_ability->damage_options_update(array(
+                    'kind' => 'energy',
+                    'kickback' => array(30, 0, 0),
+                    'success' => array(1, 10, 15, -10, 'Wow! A third hit!'),
+                    'failure' => array(1, 60, 15, -10, '')
+                    ));
+                $this_ability->recovery_options_update(array(
+                    'kind' => 'energy',
+                    'frame' => 'taunt',
+                    'kickback' => array(0, 0, 0),
+                    'success' => array(2, 10, 15, -10, 'What? The '.$this_ability->print_name().' recovered the target again!'),
+                    'failure' => array(2, 60, 15, -10, '')
+                    ));
+                $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+                
+        // Call the global stat boost function with customized options
+        rpg_ability::ability_function_stat_boost($this_robot, 'speed', 2, $this_ability);
+      
             }
+
         }
 
         // Return true on success
