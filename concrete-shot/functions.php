81346f89dbbd7c6975586a633a32d10114efca7a
Ageman20XX <adrian.marceau@gmail.com>
2023-10-06 20:21:13 -0400

    Updated Concrete Shot's data and functions

--- concrete-shot/functions.php
+++ concrete-shot/functions.php
@@ -5,32 +5,152 @@ $functions = array(
         // Extract all objects into the current scope
         extract($objects);
 
+        // Predefine attachment create and destroy text for later
+        $this_create_text = ($target_robot->print_name().' found '.$target_robot->get_pronoun('reflexive').' in a '.rpg_type::print_span('freeze', 'Frozen Foothold').'!<br /> '.
+            $target_robot->print_name().' is prevented from switching!'
+            );
+        $this_refresh_text = ($this_robot->print_name().' refreshed the '.rpg_type::print_span('freeze', 'Frozen Foothold').' at '.$target_robot->print_name().'\'s feet!<br /> '.
+            $target_robot->print_name().' is still prevented from switching!'
+            );
+
+        // Define this ability's attachment token
+        $static_attachment_key = $target_robot->get_static_attachment_key();
+        $static_attachment_duration = 3;
+        $this_attachment_info = rpg_ability::get_static_attachment($this_ability, 'frozen-foothold', $static_attachment_key, $static_attachment_duration);
+        $this_attachment_token = $this_attachment_info['attachment_token'];
+
         // Target the opposing robot
+        $this_battle->queue_sound_effect('blowing-sound');
         $this_ability->target_options_update(array(
-            'frame' => 'shoot',
-            'success' => array(0, 75, 0, 10, $this_robot->print_name().' uses the '.$this_ability->print_name().'!')
+            'frame' => $this_robot->robot_token == 'ice-man' ? 'taunt' : 'shoot',
+            'success' => array(0, 110, 0, 10, $this_robot->print_name().' uses the '.$this_ability->print_name().'!')
             ));
         $this_robot->trigger_target($target_robot, $this_ability);
 
         // Inflict damage on the opposing robot
+        $this_battle->queue_sound_effect('ice-sound');
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
-            'kickback' => array(10, 0, 0),
-            'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_name().' hit the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'kickback' => array(5, 0, 0),
+            'success' => array(1, 5, -10, 10, 'The '.$this_ability->print_name().' chilled the target!'),
+            'failure' => array(0, -65, -10, -10, 'The '.$this_ability->print_name().' missed&hellip;')
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
             'frame' => 'taunt',
-            'kickback' => array(10, 0, 0),
-            'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_name().' was absorbed by the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+            'kickback' => array(0, 0, 0),
+            'success' => array(1, 5, -10, 10, 'The '.$this_ability->print_name().' was absorbed by the target!'),
+            'failure' => array(0, -65, -10, -10, 'The '.$this_ability->print_name().' had no effect&hellip;')
             ));
         $energy_damage_amount = $this_ability->ability_damage;
         $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
 
+        // Attach the ability to the target if not disabled
+        if ($this_ability->ability_results['this_result'] != 'failure'){
+            $this_battle->queue_sound_effect('ice-sound');
+
+            // If the ability flag was not set, attach the hazard to the target position
+            if (!isset($this_battle->battle_attachments[$static_attachment_key][$this_attachment_token])){
+
+                // Attach this ability attachment to the robot using it
+                $this_battle->battle_attachments[$static_attachment_key][$this_attachment_token] = $this_attachment_info;
+                $this_battle->update_session();
+
+                // Target this robot's self
+                if ($target_robot->robot_status != 'disabled'){
+                    $this_robot->robot_frame = 'base';
+                    $this_robot->update_session();
+                    $this_ability->target_options_update(array('frame' => 'defend', 'success' => array(0, -9999, -9999, -9999, $this_create_text)));
+                    $target_robot->trigger_target($target_robot, $this_ability, array('prevent_default_text' => true));
+                }
+
+            }
+            // Else if the ability flag was set, reinforce the hazard by one more duration point
+            else {
+
+                // Collect the attachment from the robot to back up its info
+                $this_attachment_info = $this_battle->battle_attachments[$static_attachment_key][$this_attachment_token];
+                if (empty($this_attachment_info['attachment_duration'])
+                    || $this_attachment_info['attachment_duration'] < $static_attachment_duration){
+                    $this_attachment_info['attachment_duration'] = $static_attachment_duration;
+                    $this_battle->battle_attachments[$static_attachment_key][$this_attachment_token] = $this_attachment_info;
+                    $this_battle->update_session();
+                }
+                if ($target_robot->robot_status != 'disabled'){
+                    $this_ability->target_options_update(array('frame' => 'defend', 'success' => array(0, -9999, -9999, -9999, $this_refresh_text)));
+                    $target_robot->trigger_target($target_robot, $this_ability, array('prevent_default_text' => true));
+                }
+
+            }
+
+        }
+
+        // Either way, update this ability's settings to prevent recovery
+        $this_ability->damage_options_update($this_attachment_info['attachment_destroy'], true);
+        $this_ability->recovery_options_update($this_attachment_info['attachment_destroy'], true);
+        $this_ability->update_session();
+
         // Return true on success
         return true;
+
+        },
+    'ability_function_onload' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // If the user has Extended Range, allow bench targeting
+        if ($this_robot->has_attribute('extended-range')){ $this_ability->set_target('select_target'); }
+        else { $this_ability->reset_target(); }
+
+        // Return true on success
+        return true;
+
+        },
+    'static_attachment_function_frozen-foothold' => function($objects, $static_attachment_key, $this_attachment_duration = 99){
+
+        // Extract all objects and config into the current scope
+        extract($objects);
+        
+        // Generate the static attachment info using provided config
+        $existing_attachments = isset($this_battle->battle_attachments[$static_attachment_key]) ? count($this_battle->battle_attachments[$static_attachment_key]) : 0;
+        $this_ability_token = $this_ability->ability_token;
+        $this_attachment_token = 'ability_'.$this_ability_token.'_'.$this_attachment->attachment_token.'_'.$static_attachment_key;
+        $this_attachment_image = $this_ability_token;
+        $this_attachment_destroy_text = 'The <span class="ability_name ability_type ability_type_freeze">Frozen Foothold</span> trapping {this_robot} faded away... ';
+        $this_attachment_info = array(
+            'class' => 'ability',
+            'sticky' => true,
+            'ability_token' => $this_ability_token,
+            'ability_image' => $this_attachment_image,
+            'attachment_token' => $this_attachment_token,
+            'attachment_duration' => $this_attachment_duration,
+            'attachment_sticky' => true,
+            'attachment_switch_disabled' => true,
+            'attachment_weaknesses' => array('water', 'explode'),
+            'attachment_weaknesses_trigger' => 'either',
+            'attachment_destroy' => array(
+                'trigger' => 'special',
+                'kind' => '',
+                'type' => '',
+                'percent' => true,
+                'modifiers' => false,
+                'frame' => 'defend',
+                'rates' => array(100, 0, 0),
+                'success' => array(9, -9999, -9999, 10, $this_attachment_destroy_text),
+                'failure' => array(9, -9999, -9999, 10, $this_attachment_destroy_text)
+                ),
+            'ability_frame' => 2,
+            'ability_frame_animate' => array(2, 3),
+            'ability_frame_offset' => array(
+                'x' => (0 + ($existing_attachments * 8)),
+                'y' => (-5 + $existing_attachments),
+                'z' => (8 + $existing_attachments)
+                )
+            );
+
+        // Return true on success
+        return $this_attachment_info;
 
     }
 );
