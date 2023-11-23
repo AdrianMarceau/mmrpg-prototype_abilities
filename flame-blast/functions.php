98820d4440ef25f3c06d85f144926ad615787cc1
Ageman20XX <adrian.marceau@gmail.com>
2023-11-22 21:14:50 -0500

    Updated Flame Blast's functions

--- flame-blast/functions.php
+++ flame-blast/functions.php
@@ -15,19 +15,22 @@ $functions = array(
         // Inflict damage on the opposing robot
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
-            'kickback' => array(0, 0, 0),
-            'success' => array(1, 0, -10, 10, 'The '.$this_ability->print_name().' cut into the target!'),
-            'failure' => array(1, 0, -10, -10, 'The '.$this_ability->print_name().' missed&hellip;')
+            'sticky' => true,
+            'kickback' => array(5, 0, 0),
+            'success' => array(1, 0, 0, 10, 'The '.$this_ability->print_name().' burned through target!'),
+            'failure' => array(1, 0, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
+            'sticky' => true,
             'frame' => 'taunt',
-            'kickback' => array(0, 0, 0),
-            'success' => array(0, 0, 0, 10, 'The '.$this_ability->print_name().' was enjoyed by the target!'),
+            'kickback' => array(5, 0, 0),
+            'success' => array(0, 0, 0, 10, 'The '.$this_ability->print_name().' simmered the target!'),
             'failure' => array(0, 0, 0, -10, 'The '.$this_ability->print_name().' missed&hellip;')
             ));
         $energy_damage_amount = $this_ability->ability_damage;
         $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+        $first_strike_success = $this_ability->ability_results['this_result'] === 'success' ? true : false;
 
         // Initiate a second strike as long as first didn't KO the target
         if ($target_robot->robot_status != 'disabled'){
@@ -35,28 +38,30 @@ $functions = array(
             // Inflict damage on the opposing robot
             $this_ability->damage_options_update(array(
                 'kind' => 'energy',
-                'kickback' => array(10, 0, 0),
+                'sticky' => true,
+                'kickback' => array(25, 15, 0),
                 'success' => array(2, 0, 0, 10, ($first_strike_success
-									  ? 'And there\'s the second hit!'
-                    : '...but it came back for a second hit!'
+					? 'And there\'s the second hit!'
+                    : '...but it flared up for a second hit!'
                     )),
                 'failure' => array(2, 0, 0, -10, ($first_strike_success
-										? 'Oh! The second hit missed!'
+					? 'Oh! The second hit missed!'
                     : 'Oh! The second hit missed too!'
-										))
+					))
                 ));
             $this_ability->recovery_options_update(array(
                 'kind' => 'energy',
-                'kickback' => array(0, 0, 0),
+                'sticky' => true,
+                'kickback' => array(10, 5, 0),
                 'frame' => 'taunt',
-                'success' => array(2, 0, -10, 10, ($first_strike_success
-										? 'And there it goes again!' 
-                    : '...but it came back and was enjoyed by the target!!'
+                'success' => array(2, 0, 0, 10, ($first_strike_success
+					? 'And there it goes again!'
+                    : '...but it flared up again and was enjoyed by the target!!'
                     )),
-                'failure' => array(2, 0, -10, -10, ($first_strike_success 
-										? 'Oh! The second hit missed!'
-										: 'Oh! The second hit missed too!'
-										))
+                'failure' => array(2, 0, 0, -10, ($first_strike_success
+					? 'Oh! The second hit missed!'
+					: 'Oh! The second hit missed too!'
+					))
                 ));
             $energy_damage_amount = $this_ability->ability_damage2;
             $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
