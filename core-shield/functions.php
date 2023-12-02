9be040a11ed737c7c7b29c00b5aca1f1da651e3b
Ageman20XX <adrian.marceau@gmail.com>
2023-12-02 14:02:29 -0500

    Updated Core Shield's data and functions

--- core-shield/functions.php
+++ core-shield/functions.php
@@ -5,14 +5,6 @@ $functions = array(
         // Extract all objects into the current scope
         extract($objects);
 
-        // Collect or define the current "target" of this ability
-        if ($target_robot->player->player_id == $this_robot->player->player_id
-            && $target_robot->robot_id != $this_robot->robot_id){
-            $current_target_robot = $target_robot;
-        } else {
-            $current_target_robot = $this_robot;
-        }
-
         // Collect the first and second shield type, if applicable
         $first_shield_type = $this_ability->ability_type;
         $second_shield_type = false;
@@ -21,56 +13,81 @@ $functions = array(
             $second_shield_type = $this_ability->ability_type2;
         }
 
-        // Generate the first static attachment info for the robot's elemental Core Shield
-        $existing_shields = !empty($current_target_robot->robot_attachments) ? substr_count(implode('|', array_keys($current_target_robot->robot_attachments)), 'ability_core-shield_') : 0;
-        $shield_attachment_info = rpg_ability::get_static_core_shield($first_shield_type, 3, $existing_shields);
-        $shield_attachment_token = $shield_attachment_info['attachment_token'];
-        $shield_attachment = new rpg_ability($this_battle, $this_player, $this_robot, $shield_attachment_info);
-        $shield_attachment = new rpg_ability($this_battle, $this_player, $current_target_robot, $shield_attachment_info);
+        // Define a function to use for applying core shields to each individual robot
+        $apply_core_shield_to_robot = function($current_target_key, $current_target_robot)
+            use ($this_battle, $this_player, $this_robot, $this_ability,
+                $first_shield_type, $second_shield_type){
 
-        // If applicable, generate the second static attachment info for the robot's elemental Core Shield
-        if (!empty($second_shield_type)){
-            $shield2_attachment_info = rpg_ability::get_static_core_shield($second_shield_type, 3, ($existing_shields + 1));
-            $shield2_attachment_token = $shield2_attachment_info['attachment_token'];
-            $shield2_attachment = new rpg_ability($this_battle, $this_player, $this_robot, $shield2_attachment_info);
-            $shield2_attachment = new rpg_ability($this_battle, $this_player, $current_target_robot, $shield2_attachment_info);
+                // Generate the first static attachment info for the robot's elemental Core Shield
+                $existing_shields = !empty($current_target_robot->robot_attachments) ? substr_count(implode('|', array_keys($current_target_robot->robot_attachments)), 'ability_core-shield_') : 0;
+                $shield_attachment_info = rpg_ability::get_static_core_shield($first_shield_type, 3, $existing_shields);
+                $shield_attachment_token = $shield_attachment_info['attachment_token'];
+                $shield_attachment = new rpg_ability($this_battle, $this_player, $this_robot, $shield_attachment_info);
+                $shield_attachment = new rpg_ability($this_battle, $this_player, $current_target_robot, $shield_attachment_info);
+
+                // If applicable, generate the second static attachment info for the robot's elemental Core Shield
+                if (!empty($second_shield_type)){
+                    $shield2_attachment_info = rpg_ability::get_static_core_shield($second_shield_type, 3, ($existing_shields + 1));
+                    $shield2_attachment_token = $shield2_attachment_info['attachment_token'];
+                    $shield2_attachment = new rpg_ability($this_battle, $this_player, $this_robot, $shield2_attachment_info);
+                    $shield2_attachment = new rpg_ability($this_battle, $this_player, $current_target_robot, $shield2_attachment_info);
+                }
+
+                // If his is the first application of the shield, we should show the summon message too
+                if ($current_target_key === 0){
+
+                    // Attach temporarily core shields to the user for animation only
+                    $this_robot->set_attachment($shield_attachment_token.'_temp', $shield_attachment_info);
+                    if (!empty($second_shield_type)){ $this_robot->set_attachment($shield2_attachment_token.'_temp', $shield2_attachment_info); }
+
+                    // Show the summoning event for the one or two shields right now
+                    $this_battle->queue_sound_effect('charge-sound');
+                    $this_ability->target_options_update(array(
+                        'frame' => 'summon',
+                        'success' => array(0, -9999, -9999, -9999,
+                            $this_robot->print_name().' uses the '.$this_ability->print_name().' technique!'
+                            )
+                        ));
+                    $this_robot->trigger_target($this_robot, $this_ability, array('prevent_default_text' => true));
+
+                    // Remove temporary core shields from the user for animation only
+                    $this_robot->unset_attachment($shield_attachment_token.'_temp');
+                    if (!empty($second_shield_type)){ $this_robot->unset_attachment($shield2_attachment_token.'_temp'); }
+
+                }
+
+                // Now add the REAL core shields to the target (user or otherwise) and leave 'em
+                $current_target_robot->set_attachment($shield_attachment_token, $shield_attachment_info);
+                if (!empty($second_shield_type)){ $current_target_robot->set_attachment($shield2_attachment_token, $shield2_attachment_info); }
+
+                // Show the summoning event for the one or two shields right now
+                $this_battle->queue_sound_effect('small-buff-received');
+                $secondary_text = (preg_match('/^(a|e|i|o|u)/i', $first_shield_type) ? 'an' : 'a').' '.rpg_type::print_span($first_shield_type).'-type shield';
+                if (!empty($second_shield_type)){ $secondary_text = rpg_type::print_span($first_shield_type).' and '.rpg_type::print_span($second_shield_type).'-type shields'; }
+                $this_ability->target_options_update(array(
+                    'frame' => 'taunt',
+                    'success' => array(0, -9999, -9999, -9999,
+                        'The '.$this_ability->print_name().' generated '.$secondary_text.'! <br /> '.
+                        $current_target_robot->print_name().' is now protected from the element'.(!empty($second_shield_type) ? 's' : '').'!'
+                        )
+                    ));
+                $current_target_robot->trigger_target($current_target_robot, $this_ability, array('prevent_default_text' => true));
+
+
+
+            };
+
+        // Collect a list of this player's active robots so we can loop through 'em
+        $this_robots_active = $this_player->get_robots_active();
+        foreach ($this_robots_active AS $current_target_key => $current_target_robot){
+
+            // Adjust the target if it is the user themselves so we don't make duplicates
+            if ($current_target_robot->robot_id === $this_robot->robot_id){ $current_target_robot = $this_robot; }
+
+            // Apply the core shield to the current target robot
+            $apply_core_shield_to_robot($current_target_key, $current_target_robot);
+
         }
-
-        // Attach temporarily core shields to the user for animation only
-        $this_robot->set_attachment($shield_attachment_token.'_temp', $shield_attachment_info);
-        if (!empty($second_shield_type)){ $this_robot->set_attachment($shield2_attachment_token.'_temp', $shield2_attachment_info); }
-
-        // Show the summoning event for the one or two shields right now
-        $this_battle->queue_sound_effect('charge-sound');
-        $this_ability->target_options_update(array(
-            'frame' => 'summon',
-            'success' => array(0, -9999, -9999, -9999,
-                ($this_robot->robot_id !== $current_target_robot->robot_id ? ($this_robot->print_name().' targets '.$current_target_robot->print_name().'! <br /> ') : '').
-                $this_robot->print_name().' uses the '.$this_ability->print_name().' technique!'
-                )
-            ));
-        $this_robot->trigger_target($this_robot, $this_ability, array('prevent_default_text' => true));
-
-        // Remove temporary core shields from the user for animation only
-        $this_robot->unset_attachment($shield_attachment_token.'_temp');
-        if (!empty($second_shield_type)){ $this_robot->unset_attachment($shield2_attachment_token.'_temp'); }
-
-        // Now add the REAL core shields to the target (user or otherwise) and leave 'em
-        $current_target_robot->set_attachment($shield_attachment_token, $shield_attachment_info);
-        if (!empty($second_shield_type)){ $current_target_robot->set_attachment($shield2_attachment_token, $shield2_attachment_info); }
-
-        // Show the summoning event for the one or two shields right now
-        $this_battle->queue_sound_effect('small-buff-received');
-        $secondary_text = (preg_match('/^(a|e|i|o|u)/i', $first_shield_type) ? 'an' : 'a').' '.rpg_type::print_span($first_shield_type).'-type shield';
-        if (!empty($second_shield_type)){ $secondary_text = rpg_type::print_span($first_shield_type).' and '.rpg_type::print_span($second_shield_type).'-type shields'; }
-        $this_ability->target_options_update(array(
-            'frame' => 'taunt',
-            'success' => array(0, -9999, -9999, -9999,
-                'The '.$this_ability->print_name().' generated '.$secondary_text.'! <br /> '.
-                $current_target_robot->print_name().' is now protected from the element'.(!empty($second_shield_type) ? 's' : '').'!'
-                )
-            ));
-        $current_target_robot->trigger_target($current_target_robot, $this_ability, array('prevent_default_text' => true));
 
         // Return true on success
         return true;
