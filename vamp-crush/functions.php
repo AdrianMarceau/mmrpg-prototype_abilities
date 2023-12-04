557d00bea3b14dc0843a92e8271210e10b4d4565
Ageman20XX <adrian.marceau@gmail.com>
2023-12-04 15:49:38 -0500

    Updated Vamp Crush's functions

--- vamp-crush/functions.php
+++ vamp-crush/functions.php
@@ -9,27 +9,47 @@ $functions = array(
         $this_battle->queue_sound_effect('shields-down');
         $this_ability->target_options_update(array(
             'frame' => 'slide',
-            'kickback' => array(80, 0, 0),
-            'success' => array(0, 80, 0, 10, $this_robot->print_name().' uses '.$this_ability->print_name().'!')
+            'kickback' => array(90, 0, 0),
+            'success' => array(0, -20, 0, -10, $this_robot->print_name().' uses '.$this_ability->print_name().'!')
             ));
         $this_robot->trigger_target($target_robot, $this_ability);
+
+        // Move the user forward so it looks like their chowing down
+        $this_robot->set_frame('defend');
+        $this_robot->set_frame_offset('x', 180);
+        $this_robot->set_frame_styles('filter: brightness(0.1); ');
+        $target_robot->set_frame_styles('filter: brightness(0.1); ');
 
         // Inflict damage on the opposing robot
         $this_ability->damage_options_update(array(
             'kind' => 'energy',
+            'percent' => true,
+            'modifiers' => false,
             'kickback' => array(15, 0, 0),
-            'success' => array(1, -65, -10, 10, 'The '.$this_ability->print_name().' shook the very soul of the target!'),
+            'success' => array(1, -65, -10, -10, 'The '.$this_ability->print_name().' shook the very soul of the target!'),
             'failure' => array(1, -85, -5, -10, 'The '.$this_ability->print_name().' didn\'t faze the target at all&hellip;')
             ));
         $this_ability->recovery_options_update(array(
             'kind' => 'energy',
             'frame' => 'taunt',
+            'percent' => true,
+            'modifiers' => false,
             'kickback' => array(5, 0, 0),
-            'success' => array(1, -35, -10, 10, 'The '.$this_ability->print_name().' just made the target stronger! Drats!'),
+            'success' => array(1, -35, -10, -10, 'The '.$this_ability->print_name().' just made the target stronger! Drats!'),
             'failure' => array(1, -65, -5, -10, 'The '.$this_ability->print_name().' didn\'t faze the target at all&hellip;')
             ));
-        $energy_damage_amount = $this_ability->ability_damage;
-        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+        $energy_damage_percent = ($this_ability->ability_damage / 100);
+        $energy_damage_amount = round($energy_damage_percent * $target_robot->robot_energy);
+        if ($energy_damage_amount >= $target_robot->robot_energy){ $energy_damage_amount = $target_robot->robot_energy - 1; }
+        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, true, array(
+            'apply_modifiers' => false
+            ));
+
+        // Reset the user and target sprites of any changes for effect
+        $this_robot->reset_frame();
+        $this_robot->reset_frame_offset();
+        $this_robot->reset_frame_styles();
+        $target_robot->reset_frame_styles();
 
         // If the ability was a success and this robot's life energy is less than full
         if ($this_robot->robot_energy < $this_robot->robot_base_energy
@@ -41,21 +61,28 @@ $functions = array(
                 'kind' => 'energy',
                 'percent' => true,
                 'modifiers' => false,
+                'rates' => array(100, 0, 0),
                 'type' => '',
                 'frame' => 'taunt',
-                'success' => array($this_frames['target'], -9999, 5, -10, $this_robot->print_name().'&#39;s energy was restored!'),
-                'failure' => array($this_frames['target'], -9999, 5, -10, $this_robot->print_name().'&#39;s energy was not affected&hellip;')
+                'success' => array(2, -9999, 5, -10, $this_robot->print_name().'&#39;s energy was restored!'),
+                'failure' => array(2, -9999, 5, -10, $this_robot->print_name().'&#39;s energy was not affected&hellip;')
             ));
             $energy_recovery_amount = $this_ability->ability_results['this_amount'];
             $this_robot->trigger_recovery($this_robot, $this_ability, $energy_recovery_amount);
 
-      
-            }
-        },
+        }
+
+        // Return true on success
+        return true;
+
+    },
     'ability_function_onload' => function($objects){
 
         // Extract all objects into the current scope
         extract($objects);
+
+        // Make sure the front-end knows that this abilities damage is persistent
+        $this_ability->set_flag('damage_is_fixed', true);
 
         // If the user has Extended Range, allow bench targeting
         if ($this_robot->has_attribute('extended-range')){ $this_ability->set_target('select_target'); }
@@ -64,6 +91,6 @@ $functions = array(
         // Return true on success
         return true;
 
-        }
+    }
 );
 ?>
