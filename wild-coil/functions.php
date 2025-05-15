3b5a671acb7d9abe5288aff694a9e8fb3d422dca
MegaBossMan <megabossman50@gmail.com>
2025-05-15 11:06:29 -0400

    Updated Wild Coil's data, functions, and sprites

--- wild-coil/functions.php
+++ wild-coil/functions.php
@@ -6,28 +6,146 @@ $functions = array(
         extract($objects);
 
         // Target the opposing robot
+        $num_hits_counter = 0;
         $this_ability->target_options_update(array(
-            'frame' => 'shoot',
-            'success' => array(0, 75, 0, 10, $this_robot->print_name().' uses the '.$this_ability->print_name().'!')
+            'frame' => 'throw',
+            'success' => array(0, 95, 0, 10, $this_robot->print_name().' uses the '.$this_ability->print_name().'!')
             ));
-        $this_robot->trigger_target($target_robot, $this_ability);
+        $this_robot->trigger_target($target_robot, $this_ability, array('prevent_default_text' => true, 'prevent_stats_text' => true));
 
-        // Inflict damage on the opposing robot
-        $this_ability->damage_options_update(array(
-            'kind' => 'energy',
-            'kickback' => array(10, 0, 0),
-            'success' => array(1, -55, 0, 10, 'The '.$this_ability->print_name().' hit the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
-            ));
-        $this_ability->recovery_options_update(array(
-            'kind' => 'energy',
-            'frame' => 'taunt',
-            'kickback' => array(10, 0, 0),
-            'success' => array(1, -35, 0, 10, 'The '.$this_ability->print_name().' was absorbed by the target!'),
-            'failure' => array(1, -75, 0, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
-            ));
-        $energy_damage_amount = $this_ability->ability_damage;
-        $target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount);
+        // Check to see if we're there are MULTI BENCHED robots to target
+        if ($target_player->counters['robots_positions']['bench'] >= 2){
+
+            // Collect a list of benched robots from the target
+            $temp_target_robots = rpg_game::find_robots(array(
+                'player_id' => $target_player->player_id,
+                'robot_position' => 'bench',
+                'robot_status' => 'active'
+                ));
+
+            // Sort the robots by key (very important!)
+            usort($temp_target_robots, function($a, $b){
+                if ($a->robot_key < $b->robot_key){ return -1; }
+                elseif ($a->robot_key > $b->robot_key){ return 1; }
+                else { return 0; }
+                });
+
+            // Select the first target from the top of the list
+            $temp_first_target_robot = $temp_target_robots[0];
+
+            // Deal damage to the first target robot immediately
+            $this_ability->damage_options_update(array(
+                'kind' => 'energy',
+                'kickback' => array(30, 30, 0),
+                'success' => array(1, 15, 15, 10, 'The '.$this_ability->print_name().' struck the target!'),
+                'failure' => array(1, -60, 80, 10, 'The '.$this_ability->print_name().' bounced straight past the target&hellip;')
+                ));
+            $this_ability->recovery_options_update(array(
+                'kind' => 'energy',
+                'frame' => 'taunt',
+                'kickback' => array(10, 0, 0),
+                'success' => array(3, 15, 15, 10, 'The momentum of the '.$this_ability->print_name().' only made the target stronger!'),
+                'failure' => array(1, -65, -15, 10, 'The '.$this_ability->print_name().' bounced straight past the target&hellip;')
+                ));
+            $energy_damage_amount = $this_ability->ability_damage;
+            $temp_first_target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, false);
+            if ($this_ability->ability_results['this_result'] != 'failure'){ $num_hits_counter++; }
+
+            // Select the last target from the bottom of the list
+            $temp_second_target_robot = $temp_target_robots[count($temp_target_robots) - 1];
+
+            // Deal damage to the second target robot if not disabled
+            if ($temp_second_target_robot->robot_energy > 0){
+                $this_ability->ability_results_reset();
+                $this_ability->damage_options_update(array(
+                    'kind' => 'energy',
+                    'kickback' => array(-5, 30, 0),
+                    'success' => array(2, -15, -15, 10, 'The other '.$this_ability->print_name().' struck another robot!'),
+                    'failure' => array(1, -75, -15, -10, 'The '.$this_ability->print_name().' bounced straight past the target&hellip;')
+                    ));
+                $this_ability->recovery_options_update(array(
+                    'kind' => 'energy',
+                    'frame' => 'taunt',
+                    'kickback' => array(-5, 0, 0),
+                    'success' => array(4, -15, -15, -10, 'The momentum of the '.$this_ability->print_name().' only made the target stronger!'),
+                    'failure' => array(1, -75, -15, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+                    ));
+                $energy_damage_amount = $this_ability->ability_damage;
+                $temp_second_target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, false);
+                if ($this_ability->ability_results['this_result'] != 'failure'){ $num_hits_counter++; }
+            }
+            
+                                 // Call the global stat boost function with customized options
+                    rpg_ability::ability_function_stat_boost($this_robot, 'attack', 2, $this_ability, array(
+                        'initiator_robot' => $this_robot
+                        ));
+
+        }
+        // Otherwise ability will automatically target ACTIVE robot or LONE BENCHED robot
+        else {
+
+            // Define the temp target robot for the ability
+            if ($target_player->counters['robots_positions']['bench'] == 1){
+                $temp_target_robot = rpg_game::find_robot(array(
+                    'player_id' => $target_player->player_id,
+                    'robot_position' => 'bench',
+                    'robot_status' => 'active'
+                    ));
+            } else {
+                $temp_target_robot = $target_robot;
+            }
+
+            // Inflict damage on the opposing robot
+            $this_ability->damage_options_update(array(
+                'kind' => 'energy',
+                'kickback' => array(10, 20, 0),
+                'success' => array(4, 15, -10, 10, 'The '.$this_ability->print_name().' struck the target!'),
+                'failure' => array(1, -60, -10, 10, 'The '.$this_ability->print_name().' completely soared past the target&hellip;')
+                ));
+            $this_ability->recovery_options_update(array(
+                'kind' => 'energy',
+                'frame' => 'taunt',
+                'kickback' => array(10, 0, 0),
+                'success' => array(4, 15, -15, 10, 'The '.$this_ability->print_name().' just made the target stronger!'),
+                'failure' => array(1, -65, -15, 10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+                ));
+            $energy_damage_amount = $this_ability->ability_damage2;
+            $temp_target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, false);
+            if ($this_ability->ability_results['this_result'] != 'failure'){ $num_hits_counter++; }
+
+            // Inflict damage again if target not disabled
+            if ($temp_target_robot->robot_status != 'disabled'
+                && $temp_target_robot->robot_energy > 0){
+                $this_ability->ability_results_reset();
+                $this_ability->damage_options_update(array(
+                    'kind' => 'energy',
+                    'kickback' => array(-10, 40, 0),
+                    'success' => array(4, -15, -15, 10, 'The '.$this_ability->print_name().' struck the target'.($num_hits_counter > 0 ? ' again' : '').'!'),
+                    'failure' => array(1, -75, -15, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+                    ));
+                $this_ability->recovery_options_update(array(
+                    'kind' => 'energy',
+                    'frame' => 'taunt',
+                    'kickback' => array(-10, 0, 0),
+                    'success' => array(4, -15, -15, -10, 'The '.$this_ability->print_name().' made the target stronger'.($num_hits_counter > 0 ? ' again' : '').'!'),
+                    'failure' => array(1, -75, -15, -10, 'The '.$this_ability->print_name().' missed the target&hellip;')
+                    ));
+                $energy_damage_amount = $this_ability->ability_damage2;
+                $temp_target_robot->trigger_damage($this_robot, $this_ability, $energy_damage_amount, false);
+                if ($this_ability->ability_results['this_result'] != 'failure'){ $num_hits_counter++; }
+                
+                     // Call the global stat boost function with customized options
+                    rpg_ability::ability_function_stat_boost($this_robot, 'attack', 2, $this_ability, array(
+                        'initiator_robot' => $this_robot
+                        ));
+                
+            }
+
+        }
+
+        // Now that all the damage has been dealt, allow the player to check for disabled
+        $target_player->check_robots_disabled($this_player, $this_robot);
+        
 
         // Return true on success
         return true;
